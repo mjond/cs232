d9106c2348c4028e20914442996c7ffc8eceb12c
Mark Davis <mjd85@hopper.cs.calvin.edu>
2017-04-05 16:51:40 -0400

    project 4

--- project04/bakeshop.c
+++ project04/bakeshop.c
@@ -10,62 +10,126 @@ April 5, 2017
 #include <semaphore.h>
 #include <pthread.h>
 #include <errno.h>
+#include <unistd.h>
 
-bool customerWaiting;
-int customersInStore = 0;
-int availableBread = 0;
+int availableBread = 0; //tracks amount of available bread
+int totalBread = 0; //tracks total loaves baked
+int customersWaiting = 0; //tracks number of waiting customers
+long customerId; //holds customer who has customer semaphore
+int customerCheckedOut = 0;
+int breadSold = 0;
 
-//have a function for the baker checking out?
-void bakerCheckingOut() {
+sem_t customerSem; //semaphore for customer actions
+sem_t bakerSem; //semaphore for obtaining/buying bread
+sem_t storeSem; //semaphore for baker baking and checking out
 
-//process payment
-fprintf(stderr, "Baker handed out reciept");
+
+/*
+Author: Mark Davis
+parameter: none
+return: void
+function: baker checks out customers while not baking
+*/
+void* bakerCheckingOut() {
+	while (customerCheckedOut < 10) { //while not all the customers are checked out
+		if (breadSold > customerCheckedOut) { //check if the bread sold is 
+			sem_wait(&bakerSem);
+			//process payment
+			fprintf(stderr, "Baker hands out receipt to customer #%ld \n", customerId);
+			customerCheckedOut++;
+			sem_post(&bakerSem);			
+		}
+
+	}
 }
 
 
-//and one for baking bread?
-void bakeBread() {
-
+/*
+Author: Mark Davis
+parameter: none
+return: void
+function: baker bakes bread while not at cash register
+*/
+void* bakerBaking() {
+	while (totalBread < 10) { //bake bread until 10 loaves total have been made
+		sem_wait(&bakerSem); //open semaphore
+		availableBread++;
+		totalBread ++;
+		fprintf(stderr, "Loaf of bread is available \n");
+		sem_post(&bakerSem); //close semaphore
+	}
+	fprintf(stderr, "All loaves baked - baker is finished \n");
 }
 
-//then one for the customers...?
-void customer() {
-//enter store
 
-//wait for bread
+/*
+Author: Mark Davis
+parameter: Customer thread id
+return: void
+function: customers enter store when not full, obtain bread when available, 
+		pay for bread, leave store
+*/
+void *customer(void *id) {
+	long tempId; //local variable to hold customer id
+	tempId = (long)id;
 
-//obtain bread and pay for bread
+	fprintf(stderr, "Customer #%ld in the store\n", tempId);
+	sem_wait(&storeSem); //open store semaphore
 
-//
+	sem_wait(&customerSem); //open customer semaphore
+	customerId = (long)id; //store customer id in global variable for bakerCheckingOut to access
+	//if there is no bread available, wait
+	while (availableBread == 0) {
+		usleep(1);
+	}
+	fprintf(stderr, "Customer #%ld obtains a loaf of bread\n", tempId);
+	availableBread--; //one less bread is available
+	breadSold++; //add one to total bread sold
 
+	fprintf(stderr, "Customer #%ld paid\n", tempId);
+	fprintf(stderr, "Customer #%ld gets receipt\n", tempId);
+
+	//bakerCheckingOut();
+	fprintf(stderr, "Customer #%ld leaves the store\n", tempId);
+
+	//close semaphores
+	sem_post(&customerSem);
+	sem_post(&storeSem);
 }
+
 
 /*
 Author: Mark Davis
 parameter: none
 return: 0
-function: 
+function: initiate semaphores, create threads and bind them to their proper functions,
+	 exit when all threads have exited
 */
 int main() {
+	fprintf(stderr, "********* Bakery is starting up *********\n");
+
 	//initiate semaphors
-	sem_init(sem_t, )
+	sem_init(&customerSem, 0, 1);
+	sem_init(&bakerSem, 0, 1);
+	sem_init(&storeSem, 0, 5);
 
-	fprintf(stderr, "****** Bakery is starting up ******");
-	pthread_t bakingThread, cashierThread, customerThread;
+	pthread_t t[12]; ///initialize threads
+	//create threads for baker baking and checking out
+	pthread_create (&t[0], NULL, bakerBaking, NULL);
+	pthread_create (&t[1], NULL, bakerCheckingOut, NULL);
 
-	pthread_create (&thread1, NULL, customer(), NULL);
-	pthread_create (&thread2, NULL, customer(), NULL);
-	pthread_create (&thread3, NULL, customer(), NULL);
-	pthread_create (&thread4, NULL, customer(), NULL);
-	pthread_create (&thread5, NULL, customer(), NULL);
-	pthread_create (&thread6, NULL, customer(), NULL);
-	pthread_create (&thread7, NULL, customer(), NULL);
-	pthread_create (&thread8, NULL, customer(), NULL);
-	pthread_create (&thread9, NULL, customer(), NULL);
-	pthread_create (&thread10, NULL, customer(), NULL);
-
+	//create thread for each customer
+	pthread_create (&t[2], NULL, customer, (void*) 1);
+	pthread_create (&t[3], NULL, customer, (void*) 2);
+	pthread_create (&t[4], NULL, customer, (void*) 3);
+	pthread_create (&t[5], NULL, customer, (void*) 4);
+	pthread_create (&t[6], NULL, customer, (void*) 5);
+	pthread_create (&t[7], NULL, customer, (void*) 6);
+	pthread_create (&t[8], NULL, customer, (void*) 7);
+	pthread_create (&t[9], NULL, customer, (void*) 8);
+	pthread_create (&t[10], NULL, customer, (void*) 9);
+	pthread_create (&t[11], NULL, customer, (void*) 10);
 
 	pthread_exit(NULL);
 	return 0;
 }
-
